5d9d610fafd97dff9e3c7ac90c9ea90c9e803ba5
edward <edwardlfish@gmail.com>
2026-02-17 20:28:00 +0200

    Update About Me page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,45 @@
-const AboutMe = () => {
-    return (
-        <div>
-            About Me
-        </div>
-    )
-}
+import { base_url } from "../utils/constants.js";
+import { useEffect, useState } from "react";
+import mainImg from "../assets/main.jpg";
 
-export default AboutMe;
+const AboutMe = () => {
+    const [person, setPerson] = useState(null);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetch(`${base_url}/v1/peoples/1`)
+            .then(res => {
+                if (!res.ok) throw new Error(res.status);
+                return res.json();
+            })
+            .then(data => setPerson(data))
+            .catch(() => setError("Error loading About Me"));
+    }, []);
+
+    if (error) {
+        return <p className="far-galaxy">{error}</p>;
+    }
+
+    if (!person) {
+        return (
+            <p className="far-galaxy">
+                <span className="spinner-border spinner-border-sm"></span>
+            </p>
+        );
+    }
+
+    return (
+        <div className="far-galaxy">
+            <p>Name: {person.name}</p>
+            <p>Gender: {person.gender}</p>
+            <p>Height: {person.height}</p>
+            <p>Hair color: {person.hair_color}</p>
+            <p>Eye color: {person.eye_color}</p>
+            <p>
+                <img src={mainImg} alt={person.name}/>
+            </p>
+        </div>
+    );
+};
+
+export default AboutMe; AboutMe;
