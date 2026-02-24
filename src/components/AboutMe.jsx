76244466069b9f88944aa6f3057eabe498b03988
Ilia Kupitman <KupitmanIL@vk.com>
2026-02-24 06:18:44 +0200

    StarWars and About me pages update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -37,15 +37,15 @@ const AboutMe = () => {
     return (
         <>
             {(!!hero) &&
-                <div className='fs-2 lh-lg text-justify ms-5'>
-                    <p><span className='display-3'>name:</span> {hero.name}</p>
-                    <p><span className='display-3'>gender:</span> {hero.gender}</p>
-                    <p><span className='display-3'>birth year:</span> {hero.birth_year}</p>
-                    <p><span className='display-3'>height:</span> {hero.height}</p>
-                    <p><span className='display-3'>mass:</span> {hero.mass}</p>
-                    <p><span className='display-3'>hair color:</span> {hero.hair_color}</p>
-                    <p><span className='display-3'>skin color:</span> {hero.skin_color}</p>
-                    <p><span className='display-3'>eye color:</span> {hero.eye_color}</p>
+                <div className="text-3xl text-justify leading-normal tracking-widest">
+                    {Object.entries(hero).map(([key, value]) => (
+                        <p key={key}>
+                        <span className="display-3">
+                        {key.replace(/_/, " ")}:
+                        </span>{" "}
+                            {value}
+                        </p>
+                    ))}
                 </div>
             }
         </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import {navItems} from "../utils/constants.js";
 
 const Navigation = () => {
     return (
-        <nav className="fixed top-2 left-12 flex gap-4">
+        <nav className="fixed top-8 left-4 flex gap-4">
             {navItems.map(item => <NavItem itemTitle={item} key={item}/>)}
         </nav>
     )
